873d0f38b2e948d4fd40d3225c94b8cbe7a78a89
David Pujol <david.pujolperich@gmail.com>
2018-12-04 14:59:32 +0100

    Funciona la primera funcionalitat

--- Minhash.cc
+++ Minhash.cc
@@ -1,11 +1,10 @@
-#include <iostream>
-#include <set>
-#include <vector>
+#include "Minhash.h"
+
 using namespace std;
 
 #define N_DOCS 20
 
-int main() {
+void calculSimilitudMinHash() {
 	<string> shingles;
 	for(int i = 0; i < N_DOCS; i++) {
 		vector<string> paraules = llegir_document(i);
@@ -16,4 +15,15 @@ int main() {
 		cout << e << ' ';
 	cout << endl;
 	
+}
+
+
+vector <int> calcularColumna(set <string> shingles, string cjtParaules) {
+	vector <int> col(shingles.size(), 0);
+	int pos = 0;
+	for (auto s: shingles) {
+		if(cjtParaules.find(s) < cjtParaules.size()){ col[pos] = 1;  }
+		++pos;
+	}
+	return col;
 }
