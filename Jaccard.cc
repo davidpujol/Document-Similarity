58e35edbd15c526b34c9958fc41a38bee40dc705
oriolaranda <43302497+oriolaranda@users.noreply.github.com>
2018-12-04 13:27:55 +0100

    funcions jaccard

---
+++ Jaccard.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+
+
+double jaccard_index(set<string> s1, set<string> s2){
+	int size = s2.size();
+	int unio = s1.size();
+	int interseccio = 0;
+	
+	for (auto s: s2){
+		if(s1.count(s) == 1) interseccio++;
+		else unio++;
+	}
+	cout << interseccio << ", " << unio << endl;
+	return double(interseccio) / double(unio);
+}
+
+double jaccard_distance(set<string> s1, set<string> s2){
+	return 1 - jaccard_index(s1,s2);
+}
+
+int main () {
+	set<string> s1 = {"Sheldon", "Leonard", "Raj", "Howard","Penny"};
+	set<string> s2 = {"Sheldon", "Leonard", "Rash", "Howard","Penny"};
+	cout << jaccard(s1,s2) << endl;
+}
---
+++ Jaccard.hh
@@ -0,0 +1,9 @@
+#ifndef JACCARD_HH
+#define JACCARD_HH
+
+
+double jaccard_index(set<string> s1, set<string> s2);
+
+double jaccard_distance(set<string> s1, set<string> s2);
+
+#endif
