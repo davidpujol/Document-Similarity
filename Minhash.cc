0372c2b3557543b32258dac9d2e8e088371faee9
oriolaranda <oriolaranda@gmail.com>
2018-12-11 13:03:13 +0100

    oriol

--- Minhash.cc
+++ Minhash.cc
@@ -4,16 +4,31 @@ using namespace std;
 
 #define N_DOCS 20
 
+void transposada(vector<vector<int> &mat) {
+
+}
+
 void calculSimilitudMinHash() {
-	<string> shingles;
+	set<string> shingles;
+	vector<string> v;
 	for(int i = 0; i < N_DOCS; i++) {
-		vector<string> paraules = llegir_document(i);
-		set<string> s = generateKShingles(4,paraules);
-		shingles.insert(s.begin(), s.end());
-	}
-	for (auto const &e: s)
+        vector<string> paraules = llegir_document(i);
+        set<string> s = generateKShingles(4, paraules);
+        shingles.insert(s.begin(), s.end());
+
+        string cjtParaules = llegir_document(i);
+        v.push(cjtParaules);
+
+    }
+	for (auto const &e: shingles)
 		cout << e << ' ';
 	cout << endl;
+
+	vector<vector<int>> mat = vector<vector<int>>(vector<int>(shingles.size()),N_DOCS);
+	for(int i = 0; i < N_DOCS; ++i) {
+	    mat[i] = calcularColumna(shingles,v[i]);
+	}
+
 	
 }
 
@@ -27,3 +42,4 @@ vector <int> calcularColumna(set <string> shingles, string cjtParaules) {
 	}
 	return col;
 }
+
