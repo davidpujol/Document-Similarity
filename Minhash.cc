8c1cffa57fbc42e991219e5f12fa01555d1b22af
oriolaranda <oriolaranda@gmail.com>
2018-12-11 13:52:47 +0100

    oriol

--- Minhash.cc
+++ Minhash.cc
@@ -1,10 +1,10 @@
 #include "ObtenirParaules.hh"
-#include "Minhash.hh"
 #include <vector>
-
 using namespace std;
 
 #define N_DOCS 20
+
+typedef vector<vector<int>> Matriu;
 
 Matriu transposada(const Matriu & mat) {
     int cols = mat.size();
@@ -17,6 +17,20 @@ Matriu transposada(const Matriu & mat) {
     }
     return m;
 }
+
+vector <int> calcularColumna(set <string> shingles, string cjtParaules) {
+    vector <int> col(shingles.size(), 0);
+    int pos = 0;
+    for (auto s: shingles) {
+        if(cjtParaules.find(s) < cjtParaules.size())
+        {
+            col[pos] = 1;
+        }
+        ++pos;
+    }
+    return col;
+}
+
 
 void calculSimilitudMinHash() {
 	set<string> shingles;
@@ -40,16 +54,4 @@ void calculSimilitudMinHash() {
 }
 
 
-vector <int> calcularColumna(set <string> shingles, string cjtParaules) {
-	vector <int> col(shingles.size(), 0);
-	int pos = 0;
-	for (auto s: shingles) {
-		if(cjtParaules.find(s) < cjtParaules.size())
-		{
-		    col[pos] = 1;
-		}
-		++pos;
-	}
-	return col;
-}
 
--- Minhash.hh
+++ Minhash.hh
@@ -5,10 +5,6 @@
 #include <vector>
 #include "Minhash.cc"
 
-
-
-#endif //ALGORISMIA_MINHASH_H
-
 typedef vector<vector<int>> Matriu;
 
 Matriu transposada(const Matriu & mat);
@@ -17,3 +13,4 @@ void calculSimilitudMinHash();
 
 vector <int> calcularColumna(set <string> shingles, string cjtParaules);
 
+#endif
