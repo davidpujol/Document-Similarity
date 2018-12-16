9f7c92922a027eafd9afa65c14fabfa2428b82c9
David Pujol <david.pujolperich@gmail.com>
2018-12-16 18:48:43 +0100

    He millorat el mostrat de l'ultim algorisme

---
+++ generadorDocuments/creafitxers.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdlib>
+
+using namespace std;
+
+int myrandom (int i) { return std::rand()%i;}
+
+int main () {
+	ifstream file;
+  	file.open("input.txt");
+	string str;
+	vector <string> cjtParaules;
+	while (getline(file, str)) if(str.size() > 0) cjtParaules.push_back(str);
+
+	for (int i = 0; i < 20; ++i) {
+		 // using built-in random generator:
+  		random_shuffle (cjtParaules.begin(), cjtParaules.end());
+  		// using myrandom:
+  		random_shuffle (cjtParaules.begin(), cjtParaules.end(), myrandom);
+		string nom = "fitxer" + to_string(i+1)+ ".txt";
+		ofstream file(nom);
+		for(int i = 0; i < cjtParaules.size(); ++i) {
+			file << cjtParaules[i] << endl;
+		}
+		file.close();
+	}
+	
+	return 0;
+}
